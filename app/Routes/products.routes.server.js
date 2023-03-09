e482969c74efe98dc0b28489c71dc5dbf047e3c5
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-03-09 01:09:34 -0500

    Working search bar

--- app/Routes/products.routes.server.js
+++ app/Routes/products.routes.server.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import express from 'express';
+
+import Product from '../Models/products.js';
+
 import { DisplayProductList, DisplayProductAddPage, ProcessProductAddPage, DisplayProductEditPage, ProcessProductEditPage, ProcessProductDelete } from "../Controllers/products.controller.server.js";
 
 
@@ -11,5 +15,28 @@ router.post('/products-add', ProcessProductAddPage);
 router.get('/products-edit/:id', DisplayProductEditPage);
 router.post('/products-edit/:id', ProcessProductEditPage);
 router.get('/products-delete/:id', ProcessProductDelete);
+
+//SEARCH BAR
+
+// define the search route
+router.get('/search', async (req, res) => {
+  try {
+    // extract the query from the request object
+    const query = req.query.query;
+
+    // search for products that match the query
+    const products = await Product.find({
+      productName: { $regex: query, $options: 'i' },
+    });
+
+    // return the products that match the query
+    res.send(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Failed to retrieve search results');
+  }
+});
+
+
 
 export default router;
--- public/Scripts/scripts.js
+++ public/Scripts/scripts.js
@@ -25,3 +25,39 @@
         function w3_close() {
             mySidebar.style.display = "none";
         }
+
+
+        //SEARCH BAR FUNCTION:
+
+        async function handleSubmit(event) {
+          event.preventDefault(); // prevent form from reloading the page
+          const searchTerm = document.querySelector('input[name="q"]').value;
+        
+          const searchResults = await context.functions.execute("searchProducts", searchTerm);
+        
+          // update the table with search results
+          const tableBody = document.getElementById("contactList");
+          tableBody.innerHTML = "";
+          searchResults.forEach((product, index) => {
+            tableBody.innerHTML += `
+              <tr>
+                <th style="color:rgb(135, 72, 16)" scope="row" class="text-center listColor">${index + 1}</th>
+                <td style="color:rgb(135, 72, 16)">${product.productName}</td>
+                <td style="color:rgb(135, 72, 16)">${product.productID}</td>
+                <td style="color:rgb(135, 72, 16)">${product.quantityAvailable}</td>
+                <td style="color:rgb(135, 72, 16)">${product.price}</td>
+                <td style="color:rgb(135, 72, 16)">${product.quantitySold}</td>
+                <td class="text-center col-1">
+                  <a href="/products-edit/${product._id}" style="background-color: 00A491; color: aliceblue;" class="btn btn-sm">
+                    <i class="fas fa-pencil-alt"></i> Edit
+                  </a>
+                </td>
+                <td class="text-center col-1">
+                  <a href="/products-delete/${product._id}" style="background-color: 00A491; color: aliceblue;" class="btn btn-sm">
+                    <i class="fas fa-trash-alt"></i> Delete
+                  </a>
+                </td>
+              </tr>
+            `;
+          });
+        }
